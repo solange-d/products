5cc63af3460bba4299d7929808ee3dc79a764806
solange-d <solange.duemes@edu.udesc.br>
2023-06-12 13:33:46 -0300

    ProductController, ProductRepository, ProductRecodDto. POST and GET metods

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.dto.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecodDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecodDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/produtcs")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+}
